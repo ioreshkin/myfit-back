2f824cecc523f5dcdb4d54e5240d14e61f9e1bb1
sergey-oreshkin <joknife79@yandex.ru>
2024-11-13 17:23:58 +0300

    base test

--- src/main/java/center/myfit/repository/ProgramWorkoutRepository.java
+++ src/main/java/center/myfit/repository/ProgramWorkoutRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProgramWorkoutRepository extends JpaRepository<ProgramWorkout, Long> {
-    List<Program> findAllByWorkout(List<Workout> workouts);
+    List<Program> findAllByWorkoutIn(List<Workout> workouts);
 }
