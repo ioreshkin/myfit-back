6dcb40f970f7423cb5cb60bb3c02f56c21aeba2f
PavelSharko <pushkin.94.72@gmail.com>
2025-03-06 22:55:52 +0800

    добавил id в sql создания тренировки

--- src/test/java/center/myfit/controller/workout/WorkoutControllerTest.java
+++ src/test/java/center/myfit/controller/workout/WorkoutControllerTest.java
@@ -1,53 +1,60 @@
 package center.myfit.controller.workout;
 
+import static center.myfit.config.utils.ResourcePool.expected_workout_list;
 import static center.myfit.config.utils.TestConstants.API_PREFIX;
 import static center.myfit.config.utils.TestConstants.CONTENT_TYPE_JSON;
-import static center.myfit.config.utils.ResourcePool.expected_workout_list;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import center.myfit.BaseWebIntegrationTest;
+import center.myfit.config.utils.ResourcePool;
+import center.myfit.starter.dto.WorkoutDto;
 import center.myfit.starter.test.WithMockUser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.core.io.Resource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 @Transactional
-public class WorkoutEControllerTest extends BaseWebIntegrationTest {
+public class WorkoutControllerTest extends BaseWebIntegrationTest {
   private static final String BASE_URL = API_PREFIX + "/workout";
   private final ObjectMapper objectMapper = new ObjectMapper();
 
-  public WorkoutEControllerTest(WebApplicationContext context) {
+  public WorkoutControllerTest(WebApplicationContext context) {
     super(context);
   }
 
   @Test
   @WithMockUser(keycloakId = "keyclId")
-  @Sql(scripts = {"/sql/test_user.sql", "/sql/test_workouts.sql", "/sql/test_workout_exercises.sql", "/sql/test_workout_images.sql"},
-          executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+  @Sql(
+      scripts = {
+        "/sql/test_user.sql",
+        "/sql/test_workouts.sql",
+        "/sql/test_workout_exercises.sql",
+        "/sql/test_workout_images.sql"
+      },
+      executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
   void getAllWorkouts_shouldReturnAllWorkouts() throws Exception {
-    MvcResult result = mockMvc.perform(get(BASE_URL).contentType(CONTENT_TYPE_JSON))
-            .andExpect(status().isOk())
+    MvcResult result =
+        mockMvc
+            .perform(get(BASE_URL).contentType(CONTENT_TYPE_JSON))
+            .andExpect(status().isOk()) .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(2)))
             .andReturn();
 
     String jsonResponse = new String(result.getResponse().getContentAsByteArray(), StandardCharsets.UTF_8);
-    List<Map<String, Object>> actualWorkouts = objectMapper.readValue(jsonResponse, new TypeReference<>() {});
+    List<WorkoutDto> actualWorkouts =
+        objectMapper.readValue(jsonResponse, new TypeReference<>() {});
 
-    String expectedJson = new String(Files.readAllBytes(Paths.get(expected_workout_list.getURI())), StandardCharsets.UTF_8);
-    List<Map<String, Object>> expectedWorkouts = objectMapper.readValue(expectedJson, new TypeReference<>() {});
+    List<WorkoutDto> expectedWorkouts = ResourcePool.read(expected_workout_list, new TypeReference<>() {});
 
     assertThat(actualWorkouts).usingRecursiveComparison().isEqualTo(expectedWorkouts);
   }
--- src/test/resources/sql/test_workouts.sql
+++ src/test/resources/sql/test_workouts.sql
@@ -1,5 +1,5 @@
-insert into workout (title, description, owner_id, image_id)
-values ('Тренировка ног', 'с прогрессией', 1, 1);
+insert into workout (id, title, description, owner_id, image_id)
+values (1, 'Тренировка ног', 'с прогрессией', 1, 1);
 
-insert into workout (title, description, owner_id, image_id)
-values ('Тренировка груди', 'с акцентом на растяжку', 1, 2);
+insert into workout (id, title, description, owner_id, image_id)
+values (2, 'Тренировка груди', 'с акцентом на растяжку', 1, 2);
